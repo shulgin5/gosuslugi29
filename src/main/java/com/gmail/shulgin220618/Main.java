b9f7684b72d2061f96986a1b50bbe258f1a9e31d
Denis Shulgin <shulgin220618@gmail.com>
2021-02-17 20:27:47 +0300

    change name Main class and xpath for subservices class

--- src/main/java/com/gmail/shulgin220618/Main.java
+++ src/main/java/com/gmail/shulgin220618/Main.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
-public class LoginTest {
+public class Main {
 
     public static void main(String... args){
         setup();
--- src/main/java/com/gmail/shulgin220618/SubServicePage.java
+++ src/main/java/com/gmail/shulgin220618/SubServicePage.java
@@ -2,6 +2,7 @@ package com.gmail.shulgin220618;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SubServicePage {
@@ -12,17 +13,26 @@ public class SubServicePage {
         this.driver = driver;
     }
 
+    @FindBy(xpath = "//a[@class='service-organ-link']")
     private WebElement nameOrganization;
 
+    @FindBy(xpath = "//p[@class='attr-title' and contains(text(),'Государственная услуга:')]/following::p[1]")
     private WebElement nameSubService;
 
+    @FindBy(xpath = "//a[@class='reglament-link']")
     private WebElement reglamentLink;
 
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
     private WebElement buttonGet;
 
+    @FindBy(xpath = "//p[@class='attr-title' and contains(text(),'Реестровый номер услуги:')]/following::p[1]")
     private WebElement reestrNumber;
 
+    @FindBy(xpath = "//p[@class='attr-title' and contains(text(),'Идентификатор цели:')]/following::p[1]")
     private WebElement idGoal;
 
+    @FindBy(xpath = "//div[@class='mobile-hidden']")
     private WebElement dateLastUpdate;
+
+
 }
